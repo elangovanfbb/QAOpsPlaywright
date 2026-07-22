7c15b72b3add95b1b7d985a2015806cc7192664c
Elangovan Vengat <elangovan.vengat@aspiresys.com>
2026-07-22 09:40:12 +0530

    fi

--- tests/inputBox.spec.js
+++ tests/inputBox.spec.js
@@ -1,4 +1,4 @@
-const{test, expect} = require('@playwright/test')
+import { test, expect } from "@playwright/test"
 
 test.only("Input Box", async ({ page }) => {
     page.goto("https://eventhub.rahulshettyacademy.com/login")
