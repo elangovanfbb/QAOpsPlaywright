afb30cefffd88f6f71535e26a8fa82eb15aeec7b
Elangovan Vengat <elangovanven@gmail.com>
2026-07-22 19:34:15 +0530

    nav

--- tests/MethodPractice/KeyboardActions.spec.js
+++ tests/MethodPractice/KeyboardActions.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test"
 
 test("Input Box", async ({ page }) => {
-    page.goto("https://eventhub.rahulshettyacademy.com/login");
+    page.goto("https://eventhub.rahulshettyacademy.com/login")
 
    
 
